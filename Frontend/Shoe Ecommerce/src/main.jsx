7d1b4da82ce855f02880d007243d7046acda8fe8
Honey-Joe <honeyjoe942@gmail.com>
2024-12-04 12:52:32 +0530

    update

--- Frontend/Shoe Ecommerce/src/main.jsx
+++ Frontend/Shoe Ecommerce/src/main.jsx
@@ -1,12 +1,19 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 import ProductRoute from "./components/ProductRoute/ProductRoute";
 
 const AppLayout = ()=>{
   return(
     <>
-      mainroute
+      <Navbar></Navbar>
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
     </>
   )
 }
