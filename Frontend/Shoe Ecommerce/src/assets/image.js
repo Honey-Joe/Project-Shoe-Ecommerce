fe4b0ed543211c84b93f8d0a4c6836c768e578ce
Honey-Joe <honeyjoe942@gmail.com>
2024-12-11 11:12:38 +0530

    updated collection section

--- Frontend/Shoe Ecommerce/src/assets/image.js
+++ Frontend/Shoe Ecommerce/src/assets/image.js
@@ -5,3 +5,10 @@ export const navlogo = "https://ik.imagekit.io/HoneyJoe/Shoe%20Ecommerce/shoe-lo
 export const heroimg1 = "https://ik.imagekit.io/HoneyJoe/Shoe%20Ecommerce/shoe7_768x940.jpg_v=1613156880?updatedAt=1733197860039";
 export const heroimg2 = "https://ik.imagekit.io/HoneyJoe/Shoe%20Ecommerce/2021-10-16_768x940.jpg_v=1634378303?updatedAt=1733197883758";
 export const heroimg3 = "https://ik.imagekit.io/HoneyJoe/Shoe%20Ecommerce/shoe9_768x940.jpg_v=1613156880?updatedAt=1733197903942";
+
+//collectionsection
+export const collectionsection1 = "https://ik.imagekit.io/HoneyJoe/Shoe%20Ecommerce/collection3_large.jpg_v=1613156071?updatedAt=1733197969735";
+export const collectionsection2 = "https://ik.imagekit.io/HoneyJoe/Shoe%20Ecommerce/collection5_47277ffa-efcf-413d-a764-b4b5be9e4e99_large.jpg_v=1613157025?updatedAt=1733198135686";
+export const collectionsection3 = "https://ik.imagekit.io/HoneyJoe/Shoe%20Ecommerce/collection2_6fd1e403-7021-427d-8be1-8bf857c8aacf_large.jpg_v=1613157026?updatedAt=1733198157297"
+export const collectionsection4 = "https://ik.imagekit.io/HoneyJoe/Shoe%20Ecommerce/collection4_1d1d931a-3265-4eae-ac45-7e8a9cb3702e_large.jpg_v=1613157026?updatedAt=1733198176283";
+
--- Frontend/Shoe Ecommerce/src/components/Body/Body.jsx
+++ Frontend/Shoe Ecommerce/src/components/Body/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import CollectionSection from './CollectionSection/CollectionSection'
 import HeroSecrion from './HeroSection/HeroSecrion'
 
 const Body = () => {
   return (
     <div>
       <HeroSecrion></HeroSecrion>
+      <CollectionSection></CollectionSection>
     </div>
   )
 }
