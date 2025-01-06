5cdde391de7a8ec6dd7a68ebf88db1f99b903e55
Honey-Joe <honeyjoe942@gmail.com>
2025-01-06 11:10:24 +0530

    import lucide recat

--- client/src/Layouts/Navbar.jsx
+++ client/src/Layouts/Navbar.jsx
@@ -23,13 +23,11 @@ const Navbar = () => {
             class="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
+              src="https://ik.imagekit.io/HoneyJoe/Shoe%20Ecommerce/shoe-logo-new_300x300.png_v=1613157234?updatedAt=1733197131897"
               class="h-8"
               alt="Flowbite Logo"
             />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
+            
           </a>
           <button
             data-collapse-toggle="navbar-default"
@@ -57,7 +55,7 @@ const Navbar = () => {
           </button>
           <div class="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
+              <Link to={"/"}>
                 <a
                   href="#"
                   class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
@@ -65,7 +63,7 @@ const Navbar = () => {
                 >
                   Home
                 </a>
-              </li>
+              </Link>
               <li>
                 <a
                   href="#"
--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -17,7 +17,7 @@
 //     imageSrc:
 //       "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-02.jpg",
 //     imageAlt:
-//       "Olive drab green insulated bottle with flared screw lid and flat top.",
+//       "Olive drab blue insulated bottle with flared screw lid and flat top.",
 //   },
 //   {
 //     id: 3,
@@ -45,16 +45,23 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { productListAction } from "../Redux/Actions/Product";
 import { Link } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
 
 export default function Example() {
   const dispatch = useDispatch();
-  const productListReducers = useSelector((state)=> state.productListReducers)
-  const {loading, error,pages, totalPages, products=[]} = productListReducers;
+  const productListReducers = useSelector((state) => state.productListReducers);
+  const {
+    loading,
+    error,
+    pages,
+    totalPages,
+    products = [],
+  } = productListReducers;
 
-  useEffect(()=>{
-    dispatch(productListAction())
-  },[dispatch])
-  console.log(products)
+  useEffect(() => {
+    dispatch(productListAction());
+  }, [dispatch]);
+  console.log(products);
   return (
     <>
       {loading ? (
@@ -66,17 +73,36 @@ export default function Example() {
           <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
             <h2 className="sr-only">Products</h2>
 
-            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
               {products.map((product) => (
-                <Link key={product._id}  className="group" to={"details/"+product._id}>
-                  <img
-                    src={product.image}
-                    className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]"
-                  />
-                  <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                  <p className="mt-1 text-lg font-medium text-gray-900">
-                    {product.price}
-                  </p>
+                <Link
+                  key={product._id}
+                  className="group"
+                  to={"details/" + product._id}
+                >
+                  <div className=" p-5 rounded-lg shadow-2xl hover:-translate-y-2 transition">
+                    <div className="border-b">
+                      <img
+                        src={product.image}
+                        className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]"
+                      />
+                    </div>
+                    <div className="flex justify-between gap-4 flex-col">
+                      <div className="">
+                        <h3 className="mt-4 text-sm text-gray-700 font-bold line-clamp-2">
+                          {product.name}
+                        </h3>
+                        <p className="mt-1 text-lg font-medium text-gray-900">
+                          $ {product.price}
+                        </p>
+                      </div>
+                      <div className="flex justify-center">
+                        <button class="before:ease relative px-5 py-1 rounded-md overflow-hidden border border-blue-500 bg-blue-500 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-blue-500 hover:before:-translate-x-40">
+                          <span relative="relative z-10">View More</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </Link>
               ))}
             </div>
